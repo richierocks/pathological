43e6f55fbf456c15b75f5af73c63c7c9609d9b10
Janko Thyson <janko.thyson@rappster.de>
2014-05-05 16:58:09 +0200

    * /R/paths.R:   - changed own version of 'dir_copy' to 'dir_copy_rappster'; 'dir_copy'     corresponds to original version again   - changed own version of 'recompose_path' to 'recompose_path_rappster';     'recompose_path' corresponds to original version again   - some minor merge conflicts fixed:

--- R/paths.R
+++ R/paths.R
@@ -98,6 +98,80 @@ decompose_path <- function(x = dir())
   structure(decomposed_x, class = c("decomposed_path", "data.frame"))
 }
 
+#' Copy the contents of a directory
+#'
+#' Copies the contents of a directory, possibly recursively.
+#' @param source_dir String of directory to copy from.
+#' @param target_dir String of directory to copy to.
+#' @param pattern String regex or \code{NULL}. A filter for filenames, passed
+#' to \code{dir}.
+#' @param overwrite Logical value. Should existing files be overwritten?
+#' @param recursive Logical value. Should subdirectories and their contents
+#' be copied?
+#' @note Target directories that don't exist are created, silently (assuming
+#' write permission).
+#' @return A logical vector of whether or not each file was successfully
+#' copied is invisibly returned.
+#' @seealso \code{\link[base]{basename}}
+#' @examples
+#' \dontrun{
+#' #Copy subdirs by default
+#' dir_copy(R.home("etc"), file.path(tempdir(), "etc"))
+#' #Just copy the top level
+#' dir_copy(R.home("etc"), file.path(tempdir(), "etc2"), recursive = FALSE)
+#' #Now copy deeper levels, without overwriting.
+#' dir_copy(R.home("etc"), file.path(tempdir(), "etc2"), overwrite = FALSE)
+#' #Cleanup
+#' unlink(file.path(tempdir(), "etc"), recursive = TRUE)
+#' unlink(file.path(tempdir(), "etc2"), recursive = TRUE)
+#' }
+#' @export
+dir_copy <- function(source_dir, target_dir, pattern = NULL, overwrite = FALSE,
+  recursive = TRUE)
+{
+  #Retrieve all file and directory names
+  filenames <- dir(
+    source_dir,
+    pattern = pattern,
+    recursive = recursive,
+    all.files = TRUE,
+    full.names = FALSE,
+    include.dirs = TRUE
+  )
+  
+  #Create missing directories, silently.
+  is_directory <- assertive::is_dir(file.path(source_dir, filenames))
+  directories <- c(target_dir, file.path(target_dir, filenames[is_directory]))
+  plyr::tryapply(
+    directories,
+    dir.create,
+    showWarnings = FALSE,
+    recursive = recursive
+  )
+ 
+  out_dir <- file.path(target_dir, dirname(filenames[!is_directory]))
+  out_dir <- gsub("/\\.$", "", out_dir)
+  
+  if(length(out_dir) == 0) return()
+  ok <- mapply(
+    file.copy,
+    from = file.path(source_dir, filenames[!is_directory]),
+    to = out_dir,
+    overwrite = overwrite,
+    recursive = FALSE
+  )
+  if(!all(ok))
+  {
+    warning(
+      "The files ",
+      toString(sQuote(filenames[!ok])),
+      " were not copied successfully."
+    )
+  }
+  names(ok) <- filenames[!is_directory]
+  invisible(ok)
+}
+
 #' @rdname dir_copy
 #' @export
 dir_copy_rappster <- function(source_dir, target_dir, pattern = NULL, overwrite = FALSE, 
@@ -176,6 +250,30 @@ get_extension <- function(x = dir())
 recompose_path <- function(x, ...)
 {
   UseMethod("recompose_path")
+}
+
+#' @rdname decompose_path
+#' @export
+recompose_path <- function(x, ...)
+{
+  UseMethod("recompose_path")
+}
+
+#' @rdname decompose_path
+#' @method recompose_path decomposed_path
+#' @export
+recompose_path.decomposed_path <- function(x, ...)
+{
+  not_missing <- assertive::is_not_na(x[, "filename"])
+  has_an_extension <- nzchar(as.character(x[not_missing, "extension"]))
+  path <- rep.int(NA_character_, nrow(x))
+  base_x <- ifelse(
+    has_an_extension,
+    paste(x[not_missing, "filename"], x[not_missing, "extension"], sep = "."),
+    x[not_missing, "filename"]
+  )
+  path[not_missing] <- file.path(x[not_missing, "dirname"], base_x)
+  path
 }
 
 #' @rdname decompose_path
