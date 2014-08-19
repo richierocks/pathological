613a5e201987d806b0f9640ff2ed1c632080c489
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-08-19 13:32:59 +0300

    test name clarification

--- inst/tests/test_os_path.R
+++ inst/tests/test_os_path.R
@@ -1,5 +1,5 @@
 test_that(
-  "os_path works correctly",
+  "os_path works correctly with standardize = FALSE",
   {
     expected <- strsplit(Sys.getenv("PATH"), ";")[[1]]
     expect_identical(os_path(standardize = FALSE), expected)
@@ -7,7 +7,7 @@ test_that(
 )
 
 test_that(
-  "os_path works correctly",
+  "os_path works correctly with standardize = TRUE",
   {
     expected <- normalizePath(
       strsplit(Sys.getenv("PATH"), ";")[[1]], 
