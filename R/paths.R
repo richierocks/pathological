9bb1a351add412ffb939a72d81326be310436b09
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-08-21 16:55:18 +0300

    split_path now works around an R bug when setting names in lists.

--- R/paths.R
+++ R/paths.R
@@ -372,10 +372,11 @@ split_path <- function(x = dir())
   }
   original_x <- x <- coerce_to(x, "character")
   x <- standardize_path(x)
-  setNames(
-    strsplit(x, "(?<=[^/\\\\])[/\\\\]", perl = TRUE),
-    original_x
-  )
+  split_x <- strsplit(x, "(?<=[^/\\\\])[/\\\\]", perl = TRUE)
+  # setting names in a list, as of R3.1.1 processes backslashes cat-style, so 
+  # need to duplicate them
+  original_x <- str_replace_all(original_x, fixed("\\"), "\\\\")
+  setNames(split_x, original_x)
 }
 
 #' Standardize paths
