7913f7db32ffaa7e80ec30132ee8fb4a63a166fe
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-08-24 14:43:48 +0300

    Removed old comments.

--- inst/tests/test_split_path.R
+++ inst/tests/test_split_path.R
@@ -15,37 +15,6 @@ test_that(
   }
 )
 
-# test_that(
-#   "split_path works with various inputs",
-#   {
-#     x <- c(
-#       "c:/foo/bar", 
-#       "c:\\foo\\bar", 
-#       "\\\\myserver/myshare", 
-#       "~", 
-#       "a/relative/path", 
-#       "/a/path/", 
-#       ".",
-#       ".."
-#     )
-#     cfoobar <- c("c:", "foo", "bar")
-#     pwd <- strsplit(getwd(), "/")[[1]]
-#     expected <- setNames(
-#       list(
-#         cfoobar,
-#         cfoobar,
-#         c("\\\\myserver", "myshare"),
-#         strsplit(path.expand("~"), "[/\\]")[[1]],
-#         c(pwd, "a", "relative", "path"),
-#         c(get_drive(), "a", "path"),
-#         pwd,
-#         pwd[seq_len(length(pwd) - 1)]
-#       ),
-#       x
-#     )
-#   }
-# )
-
 test_that(
   "split_path works with absolute Windows paths with forward slashes.",
   {
